bc3e3dab2d66821b03616cd2e65e811d87a73db7
kgoke <kg333920@ohio.edu>
2022-09-04 22:59:29 -0400

    if statments and syntax

---
+++ 9_ifStatements.js
@@ -0,0 +1,24 @@
+// syntax
+// if (condition) { 'code'}
+
+// if statements
+var num = 8;
+if(num > 0){
+    console.log('number is positive\n');
+}
+
+// if else statements
+if (num > 0) {
+    console.log('number is positive\n');
+} else {
+    console.log('number is negative or 0\n')
+}
+
+// els if statements
+if (num > 0) {
+    console.log('number is positive\n');
+} else if (num == 0) {
+    console.log('number is 0\n')
+} else {
+    console.log('number is negative\n')
+}
