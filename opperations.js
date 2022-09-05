2a21a86cbb588ede6549d592fc49d5734aeaac7a
kgoke <kg333920@ohio.edu>
2022-09-04 20:48:55 -0400

    simple math

---
+++ opperations.js
@@ -0,0 +1,16 @@
+// addition
+var sum = 10 + 10;
+
+// subtract
+var difference = 10 - 5;
+
+// multiply
+var product = 10 * 10;
+
+// division
+var quotient = 10 / 5;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
