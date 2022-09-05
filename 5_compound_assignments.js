3645bcc014478aad4d15c71890520e6dc7acdeae
kgoke <kg333920@ohio.edu>
2022-09-04 21:22:01 -0400

    compound assignments

---
+++ 5_compound_assignments.js
@@ -0,0 +1,24 @@
+var a = 3;
+var b = 4;
+var c = 5;
+var d = 6;
+
+// additon
+a = a + 10;
+// or
+a += 10;
+
+// subtraction
+b = b - 2;
+// or
+b -= 2;
+
+// multiplication
+c = c * 2;
+// or
+c *= 2;
+
+// division
+d = d / 2;
+// or
+d /= 2;
