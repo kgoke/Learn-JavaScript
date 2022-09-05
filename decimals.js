0e9a54a1740a18471eea90afba2b424b952d390a
kgoke <kg333920@ohio.edu>
2022-09-04 21:08:23 -0400

    decimal and floating point numbers

---
+++ decimals.js
@@ -0,0 +1,10 @@
+// decimals in javascript (also known as floating point numbers)
+var myDecimal = 5.6;
+
+var myDecimal2 = 0.5;
+
+// multiplying decimal points/floating point numbers
+var product = 2.0 * 2.5; // answer is 5.0
+
+// dividing decimal points/floating point numbers
+var quotient = 5.0 / 2.5; // answer is 2.0
