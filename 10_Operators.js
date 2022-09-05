af8492310d4f994e019fa12fe018fd2f6924c30a
kgoke <kg333920@ohio.edu>
2022-09-04 23:14:19 -0400

    everything operators

---
+++ 10_Operators.js
@@ -0,0 +1,48 @@
+// regular equality operators
+/*
+
+== equal to
+
+=== equal value and equal type
+
+!= not equal
+
+!== not equal value and not equal type
+
+> greater than
+
+< less than
+
+>= greater than or equal to
+
+<= less than or equal to
+
+? ternary operator
+*/
+
+// EXAMPLES:
+
+1 == 1; // true
+
+1 == '1'; // true
+
+1 === 1; // true
+
+1 === '1'; // false
+
+1 != 1; // false
+
+1 !== '1'; // true
+
+2 > 1; // true
+
+2 < 1; // false
+
+// etc...
+
+// LOGICAL OPERATORS
+ if(1 && 2); // logical and
+
+ if(1 || 2); // logical or
+
+//  '!' logical not not
