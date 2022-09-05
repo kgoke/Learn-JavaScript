b274616b9fe49e4b4d20c90f00fe52ef7ca75611
kgoke <kg333920@ohio.edu>
2022-09-04 20:45:14 -0400

    variables

---
+++ variables.js
@@ -0,0 +1,21 @@
+// types of variables
+// undefined, null, boolean, string, symbol, number, and object
+
+// declaring a variable
+var x;  // can be used like normal variable anywhere within the program
+
+let y;  // can only be used inside the scope of something
+
+const z = 3;    // cannot be changed (also must be defined from start)
+
+// defineing a variable
+
+var a = 5;
+var b = 10;
+var c = "i am a";
+
+a = a + 1;  // 6
+b = b + 5;  // 15
+c = c + " human";   // 'i am a human'
+
+// variables in javascript are case sensitive
