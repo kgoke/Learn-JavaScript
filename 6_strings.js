0a3294a14afc9311f1c61b37aad5a46cd11052c1
kgoke <kg333920@ohio.edu>
2022-09-04 21:37:37 -0400

    everything string related

---
+++ 6_strings.js
@@ -0,0 +1,39 @@
+// examples
+
+// doubles quotation marks
+var name = "Keegan";
+
+// single quotation marks
+var animal = 'Animal';
+
+// back ticks
+var car = `Ford`;
+
+// quotation marks within a string
+
+var sent = "David asked,\" wyd? \" earlier today.";
+// or
+var sent2 = 'David asked, "wyd?" earlier today.';
+
+/*MORE SYMBOLS
+    \' single quotes
+    \" double quotes
+    \\ backslash 
+    \n newline
+    \r carriage return
+    \t tab
+    \b backspace
+    \f form fee
+*/
+
+// concatinating string
+var newstr = "hello" + "world";
+// more
+var str1 = "hello";
+str1 += "world";
+
+// findig length of strings
+var size = newstr.length;
+
+// getting characters at index in strings
+var letter = newstr[0]; // gets character at first index of string
