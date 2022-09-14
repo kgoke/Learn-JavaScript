fb2ea60d0a2944b875c9e10cfc6e39e12e983524
kgoke <kg333920@ohio.edu>
2022-09-14 00:17:36 -0400

    example of getting a strings length

--- string-length.js
+++ string-length.js
@@ -1,2 +1,5 @@
+// finding the length of a string
+// defined a string example
+// used the length function to find the length of a string
 const example = 'example string'
 console.log(example.length)
