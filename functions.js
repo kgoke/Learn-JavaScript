40d1995dd2ec1e0f654e698ae15e449c294b4f05
kgoke <kg333920@ohio.edu>
2022-09-14 00:03:56 -0400

    example of function

--- functions.js
+++ functions.js
@@ -1,3 +1,7 @@
+// example of function in javascript
+// function has the name eat
+// the argument food
+// returns the input 'food' + a string
 function eat (food) {
     return food + ' tasted really good.'
 }
