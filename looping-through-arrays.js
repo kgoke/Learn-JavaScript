279b072eb4d8ec158930d338001db2401be32b94
kgoke <kg333920@ohio.edu>
2022-09-14 00:07:16 -0400

    example of looping through an array

--- looping-through-arrays.js
+++ looping-through-arrays.js
@@ -1,3 +1,7 @@
+// example of looping through and array
+// defined the array pets with 3 values
+// loop form 0 to the length of array pets
+// adds 's' to the end of each pet name to make it plural
 let pets = ['cat', 'dog', 'rat']
 for (let i = 0; i < pets.length; i++) {
     pets[i] = pets[i] + 's'
