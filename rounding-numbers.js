033253a8cfbc7272051bea760b559767813d5a0d
kgoke <kg333920@ohio.edu>
2022-09-14 00:15:44 -0400

    example of rounding a float

--- rounding-numbers.js
+++ rounding-numbers.js
@@ -1,3 +1,6 @@
+// example of rounding a number
+// defined a varibale withe the value 1.5
+// defined a varibale thats hold the value of the first varibale rounded up 
 let roundUP = 1.5
 let round = Math.round(roundUP)
 console.log(round)
