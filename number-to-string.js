04a80c5f5cc8365422e57ad243b96c552b4d633c
kgoke <kg333920@ohio.edu>
2022-09-14 00:08:28 -0400

    converting a number to a string

--- number-to-string.js
+++ number-to-string.js
@@ -1,2 +1,5 @@
+// converting a number to a string
+// first defining a variable n to number 128
+// converst the number to a string with the toString() function
 let n = 128
 console.log(n.toString())
