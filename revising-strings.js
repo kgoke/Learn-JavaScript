581d84bb647955428737293d99bc0135aa9ab7ec
kgoke <kg333920@ohio.edu>
2022-09-14 00:14:26 -0400

    example of revising a string

--- revising-strings.js
+++ revising-strings.js
@@ -1,3 +1,6 @@
+// example of revising strings in javascript
+// declared a string pizza 
+// changed the word 'alright' to 'wonderful'
 let pizza = 'pizza is alright'
 pizza = pizza.replace('alright', 'wonderful')
 console.log(pizza)
