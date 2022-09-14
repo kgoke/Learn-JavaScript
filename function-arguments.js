75e8e289381881c05642dd5399843342ef239b5a
kgoke <kg333920@ohio.edu>
2022-09-14 00:02:53 -0400

    example of function with arguements

--- function-arguments.js
+++ function-arguments.js
@@ -1,3 +1,6 @@
+// example of function with arguments
+// we have the function math with the arguments arg1, arg2, and arg3
+// the function returns the value of arg2*arg3 added to arg1
 function math (arg1, arg2, arg3) {
     return (arg2*arg3)+arg1
 }
