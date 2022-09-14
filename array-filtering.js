48b5ab55e7908b13ff5931b5e86f2dadb51cb499
kgoke <kg333920@ohio.edu>
2022-09-13 23:58:58 -0400

    filtering array values example

--- array-filtering.js
+++ array-filtering.js
@@ -1,3 +1,6 @@
+// filtering array values example:
+// in this example we will be filtering the even values out
+// of the array numbers
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const filtered = numbers.filter(function (number) {
