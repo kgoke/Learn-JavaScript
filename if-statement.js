742aff9f87a87475c894562f5c21d19b5314368c
kgoke <kg333920@ohio.edu>
2022-09-14 00:05:20 -0400

    if-else example

--- if-statement.js
+++ if-statement.js
@@ -1,3 +1,8 @@
+// example of if else statements in javascript
+// checks if the size of fruit is greater than 5
+// if true prints a string
+// else
+// prints a different string
 let fruit = 'orange'
 if(fruit.length > 5) {
     console.log("The fruit name has more than five characters.")
