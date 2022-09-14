874aab30ae637f06c79ef96554bd07b8d24cc022
kgoke <kg333920@ohio.edu>
2022-09-14 00:13:30 -0400

    example of an object in javascript

--- objects.js
+++ objects.js
@@ -1,3 +1,7 @@
+// example of an object in javascript
+// created object pizza
+// pizza has three properties: toppings, crust, and serves
+// each property has a single or set of keys
 const pizza = {
     toppings: ['cheese', 'sauce', 'pepperoni'],
     crust: 'deep dish',
