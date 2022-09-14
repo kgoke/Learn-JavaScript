a08b513568860e286971df5b6cb1ebee774d3472
kgoke <kg333920@ohio.edu>
2022-09-14 00:00:16 -0400

    examlple of declaring an array

--- arrays.js
+++ arrays.js
@@ -1,2 +1,5 @@
+// an example of an array
+// in this example we declare and define an array pizzaToppings
+// array values and seperated by commas
 const pizzaToppings = ['tomato sauce','cheese','pepperoni']
 console.log(pizzaToppings)
